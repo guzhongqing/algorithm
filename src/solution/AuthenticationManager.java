065a35bc343e471f0b13d7175b3de88fcb1b8210
一只顾鱼 <2570174964@qq.com>
2023-02-09 22:38:00 +0800

    LeetCode： [X] 每日一题：1797. 设计一个验证系统

---
+++ src/solution/AuthenticationManager.java
@@ -0,0 +1,41 @@
+package solution;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1797. 设计一个验证系统
+ * https://leetcode.cn/problems/design-authentication-manager/
+ * 中等
+ * 哈希表
+ */
+public class AuthenticationManager {
+    int timeToLive;
+    Map<String, Integer> map;
+
+    public AuthenticationManager(int timeToLive) {
+        this.timeToLive = timeToLive;
+        this.map = new HashMap<String, Integer>();
+    }
+
+    public void generate(String tokenId, int currentTime) {
+        map.put(tokenId, currentTime + timeToLive);
+    }
+
+    public void renew(String tokenId, int currentTime) {
+        if (map.getOrDefault(tokenId, 0) > currentTime) {
+            map.put(tokenId, currentTime + timeToLive);
+        }
+    }
+
+    public int countUnexpiredTokens(int currentTime) {
+        int res = 0;
+        for (int time : map.values()) {
+            if (time > currentTime) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,15 +1,16 @@
 package test;
 
-import java.util.Arrays;
-
 class Test {
-
-
     public static void main(String[] args) {
-        String[] strings = "/a".split("/");
-        System.out.println(Arrays.toString(strings));//[, a]
-        strings = "/a/".split("/");
-        System.out.println(Arrays.toString(strings));//[, a]
+        long totalMemory = Runtime.getRuntime().totalMemory();//初始化堆内存
+        long maxMemory = Runtime.getRuntime().maxMemory();//最大堆内存
+        System.out.println(totalMemory + "B   " + totalMemory / 1024 / 1024 + "MB");
+        System.out.println(maxMemory + "B   " + maxMemory / 1024 / 1024 + "MB");
+        int[] ints = new int[(int) 2e9];
+
+
+        //HotSpot VM 参数:
+        // -Xms1024m -Xmx10240m -XX:+PrintGCDetails
     }
 
 
