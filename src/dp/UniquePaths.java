f00633670090dc4a98f2d50cf4a58ce3c9865055
一只顾鱼 <2570174964@qq.com>
2023-02-17 12:32:03 +0800

    代码随想录：62. 不同路径

---
+++ src/dp/UniquePaths.java
@@ -0,0 +1,39 @@
+package dp;
+
+/**
+ * 62. 不同路径
+ * https://leetcode.cn/problems/unique-paths/
+ * 中等
+ * dp
+ */
+public class UniquePaths {
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3, 7));
+        System.out.println(uniquePaths(3, 2));
+        System.out.println(uniquePaths(7, 3));
+        System.out.println(uniquePaths(3, 3));
+    }
+
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print(dp[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        return dp[m - 1][n - 1];
+    }
+}
