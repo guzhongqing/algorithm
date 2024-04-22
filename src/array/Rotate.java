3c7523e78f0a54021dff033e1ee4e5dc3da39d95
guzhongqing <2570174964@qq.com>
2024-04-22 18:56:47 +0800

    feat: 189. 轮转数组

--- src/array/Rotate.java
+++ src/array/Rotate.java
@@ -1,27 +1,55 @@
 package array;
 
+import java.util.Arrays;
+
+/**
+ * 189. 轮转数组
+ * https://leetcode.cn/problems/rotate-array
+ * 中等
+ * 数组，翻转数组
+ */
 public class Rotate {
 
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-
-//实现二维数组水平翻转
-        for (int i = 0; i < n / 2; i++) {
-            for (int j = 0; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n - i - 1][j];
-                matrix[n - i - 1][j] = temp;
-            }
-        }
-
-//实现二维数组对角线翻转
+    public void rotateByNew(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        // 移位到新数组
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
+            ans[(i + k) % n] = nums[i];
         }
+        // 复制回来
+        System.arraycopy(ans, 0, nums, 0, n);
+    }
 
+
+    // 通过3次翻转数组实现原地 时间n，空间1
+    public void rotateInSitu(int[] nums, int k) {
+        int n = nums.length;
+        // 这里k可能会大于数组长度，所以要mod，不然索引超了
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+
+    // [begin,end]
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] inputs = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        Rotate rotate = new Rotate();
+        rotate.rotateInSitu(inputs, k);
+        System.out.println(Arrays.toString(inputs));
     }
 }
